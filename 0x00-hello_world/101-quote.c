4e23ffa1885937f10b1ed8d0dedcc11a19b0e1d9
Samira <samira.merrakchi@hotmail.com>
2023-03-16 12:52:41 -0700

    My commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - prints out a string using write
+ * Return: int 0 when done and no errors.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	, 59);
+	return (0);
+}
