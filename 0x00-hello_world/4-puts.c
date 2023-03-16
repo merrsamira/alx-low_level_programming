7cf3348c1d4e03cf7586bc1770f696031725a7c0
Samira <samira.merrakchi@hotmail.com>
2023-03-16 12:35:27 -0700

    My commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main prints out a string via puts
+ * Return: int 0 when done and no errors
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
