1e05e6cceb647057a0bc52fd6c0ba279933d82c8
Samira <samira.merrakchi@hotmail.com>
2023-03-21 15:53:04 -0700

    ms

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * _isalpha - function that check if alphabet
+ * @c: the int cheker
+ * Return: 1 if c is alpha and 0 if no
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
