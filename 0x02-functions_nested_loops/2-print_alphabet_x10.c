79a3c7c446e9d5ff833751f41602b6a27c3efec9
Samira <samira.merrakchi@hotmail.com>
2023-03-21 15:28:54 -0700

    ms

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - function that print 10 line with alphabet
+ * Return: void funcion
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
