030184d910faf5de267f1daa17a9f7b1c73c8207
Samira <samira.merrakchi@hotmail.com>
2023-05-08 14:41:51 -0700

    first

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int opn, wrt, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		len = 0;
+		while (text_content[len])
+			len++;
+	}
+
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wrt = write(opn, text_content, len);
+
+	if (opn == -1 || wrt == -1)
+		return (-1);
+
+	close(opn);
+
+	return (1);
+}
