5eff154f5cfff4f7588a068e9d69cd2c606e00f1
merrsamira <125443409+merrsamira@users.noreply.github.com>
2023-10-14 23:28:58 +0100

    ms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,8 @@
 #include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array 
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
  *
  * @array: input array
  * @size: size of the array
